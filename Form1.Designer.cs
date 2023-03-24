edd302d5ad31c281904fd872bc36b33cf8d69d1b
lvdwiczak <76100826+lvdwiczak@users.noreply.github.com>
2023-03-24 16:01:57 +0100

    init Drag and Drop
    init Drag And Drop

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace jlDragAndDropApp
+{
+    partial class dragDropApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Capital = new System.Windows.Forms.ListBox();
+            this.Country = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Capital);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.Country);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 400;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Capital
+            // 
+            this.Capital.AllowDrop = true;
+            this.Capital.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Capital.FormattingEnabled = true;
+            this.Capital.Items.AddRange(new object[] {
+            "Warsaw",
+            "Berlin",
+            "Paris",
+            "London",
+            "Roma",
+            "Praha",
+            "Budapest",
+            "Vienna",
+            "Athens",
+            "Reykjavik"});
+            this.Capital.Location = new System.Drawing.Point(0, 0);
+            this.Capital.Name = "Capital";
+            this.Capital.Size = new System.Drawing.Size(400, 450);
+            this.Capital.TabIndex = 0;
+            this.Capital.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDrop_DragDrop);
+            this.Capital.DragOver += new System.Windows.Forms.DragEventHandler(this.DragDrop_DragOver);
+            this.Capital.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragDrop_MouseDown);
+            // 
+            // Country
+            // 
+            this.Country.AllowDrop = true;
+            this.Country.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Country.FormattingEnabled = true;
+            this.Country.Items.AddRange(new object[] {
+            "Poland",
+            "Germany",
+            "Frane",
+            "United Kingdom",
+            "Italy",
+            "Czech Republic",
+            "Hungary",
+            "Austria",
+            "Greece",
+            "Iceland"});
+            this.Country.Location = new System.Drawing.Point(0, 0);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(396, 450);
+            this.Country.TabIndex = 0;
+            this.Country.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDrop_DragDrop);
+            this.Country.DragOver += new System.Windows.Forms.DragEventHandler(this.DragDrop_DragOver);
+            this.Country.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragDrop_MouseDown);
+            // 
+            // dragDropApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "dragDropApp";
+            this.Text = "Drag & Drop by JL";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox Capital;
+        private System.Windows.Forms.ListBox Country;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jlDragAndDropApp
+{
+    public partial class dragDropApp : Form
+    {
+        public dragDropApp()
+        {
+            InitializeComponent();
+        }
+        object dragDropSource = null;
+
+        private void DragDrop_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox dragDropSender = sender as ListBox;
+            dragDropSource = dragDropSender;
+            if (e.Button == MouseButtons.Left && (dragDropSender.SelectedIndices.Count > 0))
+            {
+                DragDropEffects dragDropAction = dragDropSender.DoDragDrop(dragDropSender.SelectedItems, DragDropEffects.Copy | DragDropEffects.Move);
+                if (dragDropAction == DragDropEffects.Move)
+                    for (int index = dragDropSender.SelectedItems.Count - 1; index >= 0; index--)
+                        dragDropSender.Items.Remove(dragDropSender.SelectedItems[index]);
+            }
+            dragDropSource = null;
+        }
+
+        private void DragDrop_DragOver(object sender, DragEventArgs e)
+        {
+            if (sender == dragDropSource)
+            {
+                e.Effect = DragDropEffects.None;
+            }
+            else
+               if ((e.KeyState & 8) == 8)
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void DragDrop_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox dragDropSender = sender as ListBox;
+            ListBox dragDropSrc = dragDropSource as ListBox;
+            int indexOfPoint = dragDropSender.IndexFromPoint(dragDropSender.PointToClient(new Point(e.X, e.Y)));
+            if (indexOfPoint == -1) { indexOfPoint = dragDropSender.Items.Count; }
+            //  listBox.Items.Insert(j,e.Data.GetData(DataFormats.Text).ToString());
+            for (int index = dragDropSrc.SelectedItems.Count - 1; index >= 0; index--)
+                dragDropSender.Items.Insert(indexOfPoint,
+               dragDropSrc.Items[dragDropSrc.SelectedIndices[index]]);
+        }
+    }
+}
